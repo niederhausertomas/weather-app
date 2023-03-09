0fb3ea9117a1fe495ffa3de468619506db714fb3
Tomas Niederhauser <niederhausertomas@gmail.com>
2023-03-09 17:44:08 +0100

    weather app

---
+++ src/api.jsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8a0389502cmsh9c2051b2d4eaf03p1c4416jsnd250a36bf351',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "05088d28ab006905aa893b0df9d059f1";
